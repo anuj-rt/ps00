7d3dd0baf6904c36a7d28eb34a86f4dd3628f65e
anuj-rt <anujrt@zimbra.ccs.neu.edu>
2014-09-15 15:59:23 -0400

    Update 26.rkt

--- 26.rkt
+++ 26.rkt
@@ -2,6 +2,7 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |26|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")))))
 
+
 ;; draw-cir: List List List-> Image
 ; Given: a list of circle and positions where 
 ; Returns: image with all the circles drawn on the scene
@@ -11,6 +12,8 @@
 ; [(= (length circle-list) (length pos-list)) ...]
 ; [ else error..])
 
+
+(require 2htdp/image)
 
 (define scene (empty-scene 300 300))
 
@@ -22,17 +25,25 @@
  
  (define i 0)
 
-(define pos-list
-  (list (make-posn 20 40)
-                (make-posn 50 60)
-                (make-posn 100 200)))
+
+
+(define cir-list
+  (list 
+   
+     (circle r "solid" "blue")
+                (circle r "solid" "blue")
+                (circle r "solid" "blue")))
 
  (define (draw-cir l) 
-             (place-images (circle r "solid" "blue") l scene))
+             (place-images cir-list l scene))
  
-(draw-cir pos-list)
 
-;(draw-cir (list (make-posn 20 40)
-;                (make-posn 50 60)
-;                (make-posn 100 200))
-;          )
+
+(draw-cir (list (make-posn 20 40)
+                (make-posn 50 60)
+              (make-posn 100 200))
+        )
+(draw-cir
+  (list (make-posn 100 40)
+                (make-posn 200 60)
+                (make-posn 150 200)))
