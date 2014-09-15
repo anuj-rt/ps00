704929e453bf3dba26c0d18c5776510d5dc600df
anuj-rt <anujrt@zimbra.ccs.neu.edu>
2014-09-14 23:37:33 -0400

    Update 20.rkt

--- 20.rkt
+++ 20.rkt
@@ -1,8 +1,13 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |20|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")))))
-;; 
-(above (circle 20 "outline" "grey")
+;; human-shape: NonNegNumber NonNegNumber -> Image
+; Given: expression with the functions to create a human shape
+; Returns: image of the human shape
+; Examples:
+; human-shape => Image
+
+(define human-shape (above (circle 20 "outline" "grey")
        (beside/align "top"
         (rectangle 35 15 "solid" "yellow")
        (rectangle 50 70 "solid" "red")
@@ -11,5 +16,7 @@
         (rectangle 15 40 "solid" "yellow")
         (rectangle 15 40 "outline" "white")
         (rectangle 15 40 "solid" "yellow"))
-       )  
-
+       )) 
+       
+       
+human-shape
